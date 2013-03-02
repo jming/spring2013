bcfb4c5294f6964066622020ffca2de521858d9d
jming <jming@college.harvard.edu>
2013-03-02 15:48:46 -0500

    implement max-heapify

--- prog1/src/Heap.java
+++ prog1/src/Heap.java
@@ -2,12 +2,41 @@ import java.util.*;
 
 public class Heap {
 	
+	private ArrayList<Integer> list;
+
 	public Heap(){
-		ArrayList<int[]> list = new ArrayList<int[]>();	
+		list = new ArrayList<Integer>();	
 	}
 	
+	// swap the values in heap given two indicies
+	public void swap(int a, int b){
+		int temp = list.get(a);
+		list.set(a, list.get(b));
+		list.set(b, temp);
+	}
+	
+	// rearranges tree rooted at list.get(n) to be a maxHeap
 	public void maxHeapify(int n){
 		
+		// initialize variables
+		int l = left(list.indexOf(n));
+		int r = right(list.indexOf(n));
+		int largest = 0;
+		
+		// set largest
+		if (l < list.size() && list.get(l) > list.get(n))
+			largest = l;
+		else
+			largest = n;
+		
+		if (r < list.size() && list.get(r) > list.get(largest))
+			largest = r;
+		
+		// swap if necessary
+		if (largest != n) {
+			swap(n, largest);
+			maxHeapify(largest);
+		}
 		
 	}
 	
