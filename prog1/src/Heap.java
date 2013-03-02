87d57ec33826ba69e6ddbe0676d351dff18ca27c
Alisa <alisan011@gmail.com>
2013-03-02 15:25:56 -0500

    whooooo new class heap

---
+++ prog1/src/Heap.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Heap {
+	
+	public Heap(){
+		ArrayList<int[]> list = new ArrayList<int[]>();	
+	}
+	
+	public void maxHeapify(int n){
+		
+		
+	}
+	
+	public void buildHeap(ArrayList<int[]> list){
+		
+	}
+	
+	public void extractMax(){
+		
+	}
+	
+	public void insert(int v){
+		
+	}
+	
+
+	
+	
+
+}
