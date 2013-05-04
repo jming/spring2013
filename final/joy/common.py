3a21920338b13a932b18eff866f733d69a7591cc
jming <jming@college.harvard.edu>
2013-05-04 14:48:59 -0400

    three different classifiers, need testing

--- final/joy/common.py
+++ final/joy/common.py
@@ -1,14 +1,36 @@
 import game_interface
 import random
 import time
+from classify import *
+
 
 def get_move(view):
-  # Choose a random direction.
-  # If there is a plant in this location, then try and eat it.
-  hasPlant = view.GetPlantInfo() == game_interface.STATUS_UNKNOWN_PLANT
-  # Choose a random direction
-  if hasPlant:
-    for i in xrange(5):
-      print view.GetImage()
-  time.sleep(0.1)
-  return (random.randint(0, 4), hasPlant)
+
+    eat = 0
+
+    # 0. Land in square, check if there is a plant
+    hasPlant = view.GetPlantInfo() == game_interface.STATUS_UNKNOWN_PLANT
+
+    # If there is a plant,
+    if hasPlant:
+
+        # 1. Decide if observe/how many times
+        numobs = 5
+
+        # 2. Classify plant that many number of times
+        ispoisonous = 0
+        for i in xrange(numobs):
+            ispoisonous += classify(view.GetImage())
+
+        eat = ispoisonous / numobs > 0.5
+
+    # 3. Decide where to go
+    move = random.randint(0, 4)
+
+    # 4. Execute move
+    time.sleep(0.1)
+    return (move, eat)
+
+
+def classify(image):
+    return run_classify(image)
